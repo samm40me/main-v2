891891839c884325c01955a6e8a07bca122fe76a
Samuel Ojagbohunmi <samm40me@yahoo.com>
2022-04-25 16:15:51 -0600

    my commit

---
+++ c8-project2-client-main/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div> Calgary Shelter Occupancy</div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
